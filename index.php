ebb7a7eb3ace4bc5080500efec34be704736d23b
ilya <59298873+123ilya@users.noreply.github.com>
2022-10-13 13:00:25 +1000

    Create index.php
    first commit

---
+++ index.php
@@ -0,0 +1,188 @@
+<?php
+
+interface LoggerInterface //Интерфейс логирования.
+{
+    public function logMessage($errorText);//Записать сообщения в лог. На вход текст ошибки.
+
+    public function lastMessages($numOfMessages): array;//Получить список последних сообщений из лога
+    // На вход количество, сообщений, которые необходимо получить. Возвращает массив сообщений
+}
+
+interface EventListenerInterface//Интерфейс для обработки событий
+{
+    public function attachEvent($methodName, $function);//Присвоить событию обработчик
+    //На вход имя метода класса, при выполнении которого он должен быть вызван и колбек функцию, которая вызывается
+    //при возникновении события
+
+    public function detouchEvent($methodName);//Убрать обработчик события. Приенимает на вход имя метода класа,
+    //при выполнении которого вызывается обработчик события
+
+}
+
+
+class TelegraphText
+{
+    public string $text; //Сам текст
+    public string $title; //Заголовок текста
+    public string $author; //Автор
+    public string $published; //Дата создания объекта
+    public string $slug; //Имя файла
+
+    public function __construct($author, $slug)
+    {
+        $this->author = $author;
+        $this->slug = $slug;
+        $this->published = date('Y-m-d H:i:s');
+    }
+
+    public function storeText(): void // На основе полей объекта формирует массив, серриализует его, а затем
+        //записывает в файл.
+    {
+        $post = [
+            'title' => $this->title,
+            'text' => $this->text,
+            'author' => $this->author,
+            'published' => $this->published
+        ];
+        $serializedPost = serialize($post);
+        file_put_contents($this->slug, $serializedPost);
+    }
+
+    public function loadText() //Выгружает содержимое из файла. И на основе выгруженного массива обновляет поля объекта.
+    {
+        $loadedPost = unserialize(file_get_contents($this->slug));
+        if (filesize($this->slug) !== 0) {
+            $this->title = $loadedPost['title'];
+            $this->text = $loadedPost['text'];
+            $this->author = $loadedPost['author'];
+            $this->published = $loadedPost['published'];
+            return $this->text;
+        }
+    }
+
+    public function editText($title, $text): void//Изменяет содержимое полей объекта title и text
+    {
+        $this->title = $title;
+        $this->text = $text;
+    }
+}
+
+
+//1.Абстрактный класс для хранилища
+abstract class Storage implements LoggerInterface, EventListenerInterface
+{
+    public function attachEvent($methodName, $function)
+    {
+        // TODO: Implement attachEvent() method.
+    }
+
+    public function detouchEvent($methodName)
+    {
+        // TODO: Implement detouchEvent() method.
+    }
+
+    public function lastMessages($numOfMessages): array
+    {
+        // Не понимаю, как получить список последних сообщений из лога, если все сообщения пишуться
+        //в одну строку. В итоге лог - это одна большая строка. Между строками отсутствуют разделители
+    }
+
+    public
+    function logMessage(
+        $errorText
+    ) {
+        error_log($errorText, 3, 'error_log');//1-й аргумент сообщение об ошибке, которое должно быть логировано
+        //2-й аргумент определяет, куда отправлять ошибку. (3 - применяется к указанному в destination файлу.)
+        //3-й аргумент назначение (файл, куда записываются ошибки)
+    }
+
+    abstract public function create(&$object);//создает объект в хранилище
+
+    abstract public function read($slug): object;//получает объект из хранилища
+
+    abstract public function update($slug, $object);//обновляет существующий объект в хранилище
+
+    abstract public function delete($slug);//удаляет объект из хранмилища
+
+    abstract public function list_(): array;//возвращает массив всех объектов в хранилище
+}
+
+//2. Абстрактный класс для представления
+abstract class View
+{
+    public object $storage;
+
+//    public function _construct($object)
+//    {
+//        $this->storage = $object;//Присваивает полю $storage значение объекта, созданного подклассом Storage
+//    }
+
+    abstract public function displayTextById($id);//Выводит текст по id
+
+    abstract public function displayTextByUrl($url);//Выводитт текст по url
+
+}
+
+//3.Абстрактный класс User
+abstract class User
+{
+    public string $id, $name, $role;
+
+    abstract public function getTextToEdit();//Выводит список текстов, доступных пользователю для редактирования
+}
+
+class FileStorage extends Storage // Метод серриализует и записывает в файл, объект класса TelegraphText
+{
+    public function create(&$object): string
+    {
+        $count = 1;
+        $fileName = $object->slug . '_' . date('Y-m-d');
+        $name = $fileName;
+        while (file_exists($name)) {
+            $name = $fileName . '_' . $count;
+            $count++;
+        }
+        $object->slug = $name;
+        $serializedObject = serialize($object);
+        file_put_contents($object->slug, $serializedObject);
+        return $object->slug;
+    }
+
+    public function delete($slug) // Удаляет файл с именем $slug
+    {
+        unlink($slug);
+    }
+
+    public function list_(
+    ): array //Возвращает массив объектов, полученных при дессиаризации содержимого файлов в дирректории.
+    {
+        $resultList = [];//Результирующий массив
+        $list = scandir('./');//Перечень всех файлов и папок, находящихся в дирректории
+        foreach ($list as $item) {
+            if ($item !== '.' && $item !== '..' && !is_dir($item) && $item !== 'index.php') {
+                $content = file_get_contents($item);
+                $resultList[] = unserialize($content);
+            }
+        }
+        return $resultList;
+    }
+
+    public function read($slug): object //Возвращает дессиаризованный объект из файла с именем $slug
+    {
+        return unserialize(file_get_contents($slug));
+    }
+
+    public function update($slug, $object) //Перезаписывает файл с именем $slug серриализованным объектом $object
+    {
+        $serializedObject = serialize($object);
+        file_put_contents($slug, $serializedObject);
+    }
+
+}
+
+//--------------------------------------------------------------------------
+//$text = new TelegraphText('ilya', 'test');
+//$text->editText('testTitle', 'swlihsliuhfwleihfliwuhfwleiuhfliwuhfliwuhfwleihf');
+//
+//$testStore = new FileStorage();
+//$testStore->create($text);
